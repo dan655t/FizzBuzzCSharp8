49c737574bf789a6b36b73c2816206fcc5c141c5
dan655t <dan655t@gmail.com>
2019-04-07 21:18:19 -0400

    initial commit

---
+++ FizzBuzzCSharp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzzCSharp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable
+                .Range(1, 100)
+                .ToList()
+                .ForEach(n => Console.WriteLine(FizzBuzz(n)));
+        }
+
+        private static string FizzBuzz(int n)
+            => (n % 3, n % 5, n) switch
+        {
+            (0, 0, _) => "FizzBuzz",
+            (0, _, _) => "Fizz",
+            (_, 0, _) => "Buzz",
+            (_, _, _) => n.ToString()
+        };
+    }
+}
